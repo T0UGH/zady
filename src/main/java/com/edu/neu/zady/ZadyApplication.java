48705728628ae95daca9b94302dea12553a7b847
t0ugh <1243384249@qq.com>
2020-03-16 09:38:26 +0800

    Feat:init project

---
+++ src/main/java/com/edu/neu/zady/ZadyApplication.java
@@ -0,0 +1,13 @@
+package com.edu.neu.zady;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadyApplication.class, args);
+    }
+
+}
