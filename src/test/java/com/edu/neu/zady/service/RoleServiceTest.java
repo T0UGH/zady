805c5be142eff612aba2d805c64ffb4d7b80f3a6
t0ugh <1243384249@qq.com>
2020-04-04 10:11:44 +0800

    Feat:RoleServiceTest

--- src/test/java/com/edu/neu/zady/service/RoleServiceTest.java
+++ src/test/java/com/edu/neu/zady/service/RoleServiceTest.java
@@ -1,11 +1,17 @@
 package com.edu.neu.zady.service;
 
 import com.edu.neu.zady.ZadyApplicationTests;
+import com.edu.neu.zady.exception.BadDataException;
+import com.edu.neu.zady.pojo.Project;
+import com.edu.neu.zady.pojo.Role;
+import com.edu.neu.zady.pojo.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,10 +29,62 @@ class RoleServiceTest extends ZadyApplicationTests {
     }
 
     @Test
-    void inviteUser(){
-        Integer projectId = 1;
-        Integer userId = 9;
+    void inviteUserAndJoin(){
+        Integer projectId = 3;
+        Integer userId = 1;
         int affectRow = roleService.inviteUser(projectId, userId);
-        Assertions.assertEquals(1, affectRow);
+        assertEquals(1, affectRow);
+        affectRow = roleService.joinProject(projectId, userId);
+        assertEquals(1, affectRow);
+    }
+
+    @Test
+    void inviteUser2(){
+        try{
+            Integer projectId = 3;
+            Integer userId = 2;
+            roleService.inviteUser(projectId, userId);
+            fail();
+        }catch (BadDataException e){
+            assertNotNull(e.getMessage());
+        }
+    }
+
+    @Test
+    void deleteUser(){
+        int affectRow = roleService.deleteUser(1, 9);
+        assertEquals(1, affectRow);
+    }
+
+    @Test
+    void updateByPIdAndUId(){
+        Integer projectId = 3;
+        Integer userId = 1;
+        String role = "developer";
+        int affectRow = roleService.updateByPIdAndUId(projectId, userId, role);
+        assertEquals(1, affectRow);
+        Role dbRole = roleService.selectByPIdAndUId(projectId, userId);
+        assertEquals(dbRole.getRole(), role);
+    }
+
+    @Test
+    void selectUsersByPId(){
+        Integer projectId = 3;
+        List<User> userList= roleService.selectUsersByPId(projectId);
+        assertNotEquals(0, userList.size());
+    }
+
+    @Test
+    void selectInvitesByPId(){
+        Integer projectId = 1;
+        List<User> userList= roleService.selectInviteUsersByPId(projectId);
+        assertEquals(0, userList.size());
+    }
+
+    @Test
+    void selectProjectsByUId(){
+        Integer userId = 1;
+        List<Project> projectList= roleService.selectProjectsByUId(userId);
+        assertNotEquals(0, projectList.size());
     }
 }
