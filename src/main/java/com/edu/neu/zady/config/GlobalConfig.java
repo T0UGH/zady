05d08842456639ae54d38fd7c78bbc88d985cf75
t0ugh <1243384249@qq.com>
2020-04-30 18:29:44 +0800

    Fix:彻底解决不解析String的问题

--- src/main/java/com/edu/neu/zady/config/GlobalConfig.java
+++ src/main/java/com/edu/neu/zady/config/GlobalConfig.java
@@ -4,6 +4,7 @@ import com.edu.neu.zady.interpretor.AuthenticationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -29,8 +30,9 @@ public class GlobalConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 
-        converters.remove(4);
-        converters.remove(4);
+        converters.removeIf(converter -> converter instanceof StringHttpMessageConverter);
+//        converters.remove(4);
+//        converters.remove(4);
         converters.add(0, new MappingJackson2HttpMessageConverter());
 
     }
